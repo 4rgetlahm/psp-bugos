9e660c400b15b5193a39824187f1ade1e5091f71
Arnas Verpečinskas <arnas.verpecinskas@unity3d.com>
2022-11-10 23:36:43 +0200

    Add account endpoints

---
+++ psp-bugos/Controllers/Account/AccountManagementController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace psp_bugos.Controllers.Account
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountManagementController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> EditEmployeePrivileges(dynamic employeePrivilegesEditRequest)
+        {
+            return Ok();
+        }
+    }
+}
