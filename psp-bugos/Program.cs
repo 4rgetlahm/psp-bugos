762eaf4f19248a433b8c9f203afacdb33b5c73db
ArmintasP <75906786+ArmintasP@users.noreply.github.com>
2022-11-19 04:36:28 +0200

    Initial commit.

--- psp-bugos/Program.cs
+++ psp-bugos/Program.cs
@@ -30,11 +30,8 @@ builder.Services.AddSingleton<IRandomDataGenerator, RandomDataGenerator>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
